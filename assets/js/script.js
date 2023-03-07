8d793605cf2ad057fa2f2338d6c328a7f0e68cf6
Alexander Glemme <113175237+alexanderglemme@users.noreply.github.com>
2023-03-07 20:48:25 +0000

    Add error handling, and API key status modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,29 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
-    const `${API_URL}?api_key=${API_KEY}`
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        displayError(data);
+        throw new Error(data.error);
+
+    }
+}
+
+function displayStatus(data) {
+    document.getElementById("resultsModalTitle").innerText = "API key status:"
+    document.getElementById("results-content").innerHTML = `<div>Your key is valid until: ${data.expiry}</div>`
+    resultsModal.show()
+}
+
+function displayError(data) {
+    document.getElementById("resultsModalTitle").innerText = "API key status:"
+    document.getElementById("results-content").innerHTML = `<div>${data.error}. <br>Please go to:<br>https://ci-jshint.herokuapp.com/ to find out more.</div>`
+    resultsModal.show()
 }
