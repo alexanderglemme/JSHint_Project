f14df4c5cd20646e5ec780b844fe6adf92d3d4d6
Alexander Glemme <113175237+alexanderglemme@users.noreply.github.com>
2023-03-08 08:04:33 +0000

    Add functions tha post and display errors and API key status in modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,26 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e))
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: { "Authorization": API_KEY, },
+        body: form,
+    })
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+
+}
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -14,20 +34,45 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
-        displayError(data);
+        displayApiKeyError(data);
         throw new Error(data.error);
 
     }
+
+}
+
+function displayErrors(data) {
+
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 function displayStatus(data) {
-    document.getElementById("resultsModalTitle").innerText = "API key status:"
+    document.getElementById("resultsModalTitle").innerText = "API Key Status:"
     document.getElementById("results-content").innerHTML = `<div>Your key is valid until: ${data.expiry}</div>`
     resultsModal.show()
+
 }
 
-function displayError(data) {
-    document.getElementById("resultsModalTitle").innerText = "API key status:"
+function displayApiKeyError(data) {
+    document.getElementById("resultsModalTitle").innerText = "API Key Error:"
     document.getElementById("results-content").innerHTML = `<div>${data.error}. <br>Please go to:<br>https://ci-jshint.herokuapp.com/ to find out more.</div>`
     resultsModal.show()
+
 }
+
