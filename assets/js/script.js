66c8bf8846b9639810155702ee6dd7d6223f259c
Alexander Glemme <113175237+alexanderglemme@users.noreply.github.com>
2023-03-08 08:14:38 +0000

    Add function to process options data to right format

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,23 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e))
 
+function processOptions(form) {
+    let optArray = [];
+
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
+        }
+    }
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+    
+}
+
 async function postForm(e) {
     const form = new FormData(document.getElementById("checksform"));
 
@@ -36,7 +53,6 @@ async function getStatus(e) {
     } else {
         displayApiKeyError(data);
         throw new Error(data.error);
-
     }
 
 }
@@ -60,6 +76,7 @@ function displayErrors(data) {
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
+
 }
 
 function displayStatus(data) {
